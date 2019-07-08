19f07ec109a8f30d4a8dcbedd7bb35bf97935fa6
maneesh <kumarmaneesh.tech@gmail.com>
2019-07-08 16:39:52 +0800

    delete unnecessary files

---
+++ ip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker inspect -f '{{.Name}}-{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep slave > test.txt
+
+arr=()
+input="test.txt"
+while IFS= read -r line
+do
+  #s = "$line"
+  A="$(cut -d'-' -f3 <<<"$line")"
+  #echo "$A"
+  arr+="$A,"
+done < "$input"
+echo ${arr[@]}
